caaba564ab446e5f93f5744943ff11d0b4e02054
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-03-19 13:52:14 +0100

    Revert "Warn when multiple versions and/or sets of each type is given"
    This reverts commit 2e79702f3a32877f1badd53f9b48fb001d41dfde.

--- src/phpDocumentor/Pipeline/Stage/InitializeBuilderFromConfig.php
+++ src/phpDocumentor/Pipeline/Stage/InitializeBuilderFromConfig.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace phpDocumentor\Pipeline\Stage;
 
-use OutOfRangeException;
 use phpDocumentor\Configuration\VersionSpecification;
 use phpDocumentor\Descriptor\ApiSetDescriptor;
 use phpDocumentor\Descriptor\Collection;
@@ -23,7 +22,6 @@ use phpDocumentor\Descriptor\GuideSetDescriptor;
 use phpDocumentor\Descriptor\VersionDescriptor;
 use phpDocumentor\Guides\Nodes\ProjectNode;
 
-use function count;
 use function md5;
 
 final class InitializeBuilderFromConfig
@@ -53,9 +51,6 @@ final class InitializeBuilderFromConfig
     private function buildVersion(VersionSpecification $version): VersionDescriptor
     {
         $collection = Collection::fromClassString(DocumentationSetDescriptor::class);
-
-        $this->guardAgainstMultipleSetsOfTheSameType($version);
-
         foreach ($version->getGuides() as $guide) {
             $collection->add(
                 new GuideSetDescriptor(
@@ -83,49 +78,5 @@ final class InitializeBuilderFromConfig
             $version->getNumber(),
             $collection,
         );
-    }
-
-    /**
-     * Until official support is added for versions, check whether there is 1 and fail when multiple is
-     * given.
-     *
-     * By adding this restriction, it should prevent confusion with users when they were to add multiple
-     * in the configuration.
-     */
-    private function guardAgainstMultipleSetsOfTheSameType(VersionSpecification $version): void
-    {
-        if (count((array) $version->getGuides()) > 1) {
-            throw new OutOfRangeException(
-                <<<'EOF'
-phpDocumentor supports 1 set of guides at the moment, support for multiple 
-sets is being worked on.
-
-If you see this message, it may be that you have defined multiple "<guide>"
-elements in your configuration file.
-
-To fix this, make sure you only have one "<guide>" element in your
-documentation.
-EOF,
-            );
-        }
-
-        if (count($version->getApi()) > 1) {
-            throw new OutOfRangeException(
-                <<<'EOF'
-phpDocumentor supports 1 set of API documentation at the moment, support for 
-multiple sets is being worked on.
-
-If you see this message, it may be that you have defined multiple "<api>" 
-elements in your configuration file, or because you have used absolute paths
-as value for the "-d" or "--directory" argument. phpDocumentor interprets 
-absolute paths as separate components, each with their own set of 
-documentation.
-
-To fix this, make sure you only have one "<api>" element in your documentation
-and/or use relative paths when using the "-d" or "--directory" command line
-argument.
-EOF,
-            );
-        }
     }
 }
